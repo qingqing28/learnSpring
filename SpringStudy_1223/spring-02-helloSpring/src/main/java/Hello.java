7bbed4d267891d5effbd186062e022ff7d0bf2f4
PQQ <qqpengyc@163.com>
2020-12-29 22:34:55 +0800

    以hellospring为例认识spring

---
+++ SpringStudy_1223/spring-02-helloSpring/src/main/java/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    private String str ;
+    public String getStr(){
+        return str ;
+    }
+    public void setStr(String str){
+        this.str = str ;
+    }
+    public String toString(){
+        return "Hello{str=‘"+str+"\'}";
+    }
+}
---
+++ SpringStudy_1223/spring-02-helloSpring/src/test/java/test.java
@@ -0,0 +1,10 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class test {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Hello hello =(Hello) context.getBean("hello");
+        System.out.println(hello.toString());
+    }
+}
